1031914822726f4d141781e2360876bb2a12ce6b
xjflyttp <xjflyttp@gmail.com>
2015-07-27 13:51:45 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # yii2-metroui
+```
 https://github.com/olton/Metro-UI-CSS
+```
+
+composer.json
+---------
+```json
+"require": {
+    "xj/yii2-metroui": "*"
+},
+```
---
+++ assets/MetroUiAsset.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace xj\metro;
+
+use yii\web\AssetBundle;
+
+/**
+ *
+ * @author xjflyttp <xjflyttp@gmail.com>
+ */
+class MetroUiAsset extends AssetBundle
+{
+
+    public $sourcePath = '@bower/metro/build';
+    public $basePath = '@webroot/assets';
+    public $js = [
+        'js/metro.js',
+    ];
+    public $css = [
+        'css/metro.css',
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+
+}
